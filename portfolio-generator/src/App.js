2d4b14e4d6b17b82bc48bf621d5cedeaa0b939af
Arunit <vyasmiri21@gmail.com>
2025-06-14 20:31:08 +0530

    Fixed

--- portfolio-generator/src/App.js
+++ portfolio-generator/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ResumeBuilder from './pages/ResumeBuilder';
 import ProjectsPage from './pages/ProjectPage';
